806158430cd9ee51b600f101af32a4d77eafa670
amaljose <amaljvattakkunnel@gmail.com>
2023-12-19 11:29:12 +0530

    privacy policy updated

--- lib/screens/student/settings/privacy_screen.dart
+++ lib/screens/student/settings/privacy_screen.dart
@@ -27,13 +27,22 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ],
               ),
               SizedBox(height: 8.0),
-              Text(
-                'Effective Date: 18-12-2023',
-                style: TextStyle(
-                  color: Colors.grey, 
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Effective Date: 18-12-2023',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
               SizedBox(height: 16.0),
+              Text(
+                  '''Thank you for using the EDU PLAN mobile application, developed by FlutterVerseOfficial ("Amal Jose"). EDU PLAN is designed for teachers and students within a specific school, and this Privacy Policy outlines how your data is collected, used, and protected. By using the App, you agree to the terms outlined in this Privacy Policy.''',
+                  textAlign: TextAlign.justify),
+              SizedBox(height: 8.0),
               Text(
                 '1. About App:',
                 style: TextStyle(
@@ -43,8 +52,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
               ),
               SizedBox(height: 8.0),
               Text(
-                '''This is not an app for common users. This is only for a school not for everyone so If you don’t have access you cant login the app. So if anyone downloads the app and signs up the data not stored on the database permanently it will be removed by the admin but it stores first but the data can view and control the school admin only. If you want to connect the school admin it's given below the contact section.''',
-              ),
+                  '''This app is designed exclusively for school use and is not intended for general users. Only those with authorized access can log in to the app. If someone downloads the app and signs up, their data will be temporarily stored in the database, but it will be removed permanently by the admin. Initially, the data is stored, but only the school admin has the ability to view and control it. For inquiries or to connect with the school admin, please refer to the contact information provided below.''',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
               Text(
                 '2. Information We Collect:',
@@ -61,8 +70,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                '''The app only collects the teacher's name, email, password and contact number on sign up, it's just a request but the data is not stored permanently on the database if you are not a teacher in that school. The data goes as a request to the admin and if the admin verifies this is a teacher and accepts the request then only the data stored on the database else admin rejects the request means the data will clear.''',
-              ),
+                  '''The app only collects the teacher's name, email, password, and contact number during the sign-up process. However, this information is treated as a request, and it is not stored permanently in the database if the individual is not a teacher at that particular school. The data is sent to the admin as a request, and if the admin verifies that the individual is a teacher and accepts the request, only then is the data stored in the database. Otherwise, if the admin rejects the request, the data will be cleared.''',
+                  textAlign: TextAlign.justify),
               Text(
                 '2.2 Students Data:',
                 style: TextStyle(
@@ -70,8 +79,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                '''Students or any other users are not providing any details directly to the app,they can only login if you are a student. Students personal details and credit details are already then given to the school administration and they have the authority to use the data. The authority is giving the data to teachers. They create the accounts for students and the password also ,so students or any other user can't login without the email and password. Students give email directly to teachers so they know the teacher and the school have their data. Teachers create and give the password to students so others can't use the app.''',
-              ),
+                  '''Students or any other users do not directly provide any details to the app; they can only log in if they are a student. Personal details and credit information for students are already provided to the school administration, and they have the authority to use this data. The administration, in turn, is responsible for providing the data to teachers. Teachers are tasked with creating accounts for students and generating passwords. As a result, students or any other users cannot log in without the corresponding email and password. Students directly provide their email to teachers, ensuring that teachers are aware of the student and that the school possesses their data. Teachers are responsible for creating and distributing passwords to students to prevent unauthorized access to the app.''',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
               Text(
                 '2.3 School Administration Data:',
@@ -80,8 +89,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The App does not collect data related to the school administration, such as the name and contact details of the school principal or head of the school.They have only the control of the app. This is only for educational purposes.',
-              ),
+                  'The app does not collect data related to the school administration, such as the name and contact details of the school principal or head of the school. They only have control over the app, and it is intended solely for educational purposes.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 16.0),
               Text(
                 '3. Data Control:',
@@ -98,8 +107,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The collected data is under the control of the school administrator, who is typically the principal or head of the school. The administrator has exclusive access to and control over user data.',
-              ),
+                  'The collected data is under the control of the school administrator, who is typically the principal or head of the school. The administrator has exclusive access to and control over user data.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
               Text(
                 '3.2 Developer Access:',
@@ -108,8 +117,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The Developer, FlutterVerseOfficial, does not have access to the user data collected by the App. All data is solely controlled and managed by the school administrator.',
-              ),
+                  'The Developer, FlutterVerseOfficial, does not have access to the user data collected by the App. All data is solely controlled and managed by the school administrator.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 16.0),
               Text(
                 '4. Data Usage:',
@@ -126,8 +135,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The collected data is used for educational purposes within the school environment. This includes facilitating communication between teachers and students, managing academic activities, and other educational functionalities.',
-              ),
+                  'The collected data is used for educational purposes within the school environment. This includes facilitating communication between teachers and students, managing academic activities, and other educational functionalities.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
               Text(
                 '4.2 No Data Sharing:',
@@ -136,8 +145,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The App does not share user data with any external third parties, including the Developer. The data is strictly controlled within the school environment.',
-              ),
+                  'The App does not share user data with any external third parties, including the Developer. The data is strictly controlled within the school environment.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 16.0),
               Text(
                 '5. Data Security:',
@@ -154,8 +163,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'The App employs industry-standard encryption protocols to secure user data during transmission and storage.',
-              ),
+                  'The App employs industry-standard encryption protocols to secure user data during transmission and storage.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 16.0),
               Text(
                 '6. Contact Information:',
@@ -172,8 +181,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'For any questions or concerns regarding privacy, you can contact School Admin at amaljvattakkunnel@gmail.com.',
-              ),
+                  'For any questions or concerns regarding privacy, you can contact School Admin at amaljvattakkunnel@gmail.com.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
               Text(
                 '6.2 Contacting the Developer:',
@@ -182,8 +191,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'For any questions or concerns regarding privacy, you can contact FlutterVerseOfficial at flutterverseofficial@gmail.com.',
-              ),
+                  'For any questions or concerns regarding privacy, you can contact FlutterVerseOfficial at flutterverseofficial@gmail.com.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 16.0),
               Text(
                 '7. Consent:',
@@ -193,8 +202,8 @@ class ScreenPrivacyPolicyStudent extends StatelessWidget {
                 ),
               ),
               Text(
-                'By using the EDU PLAN App, you consent to the terms outlined in this Privacy Policy.',
-              ),
+                  'By using the EDU PLAN App, you consent to the terms outlined in this Privacy Policy.',
+                  textAlign: TextAlign.justify),
               SizedBox(height: 8.0),
             ],
           ),
--- lib/screens/welcome/privacy_screen.dart
+++ lib/screens/welcome/privacy_screen.dart
@@ -47,11 +47,19 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ],
                         ),
                         SizedBox(height: 8.0),
-                        Text(
-                          'Effective Date: 18-12-2023',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right:10.0), 
+                              child: Text(
+                                'Effective Date: 18-12-2023',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                         SizedBox(height: 16.0),
                         Text(
@@ -63,8 +71,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                         ),
                         SizedBox(height: 8.0),
                         Text(
-                          '''This is not an app for common users. This is only for a school not for everyone so If you don’t have access you cant login the app. So if anyone downloads the app and signs up the data not stored on the database permanently it will be removed by the admin but it stores first but the data can view and control the school admin only. If you want to connect the school admin it's given below the contact section.''',
-                        ),
+                            '''This app is designed exclusively for school use and is not intended for general users. Only those with authorized access can log in to the app. If someone downloads the app and signs up, their data will be temporarily stored in the database, but it will be removed permanently by the admin. Initially, the data is stored, but only the school admin has the ability to view and control it. For inquiries or to connect with the school admin, please refer to the contact information provided below.''',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 8.0),
                         Text(
                           '2. Information We Collect:',
@@ -81,8 +89,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          '''The app only collects the teacher's name, email, password and contact number on sign up, it's just a request but the data is not stored permanently on the database if you are not a teacher in that school. The data goes as a request to the admin and if the admin verifies this is a teacher and accepts the request then only the data stored on the database else admin rejects the request means the data will clear.''',
-                        ),
+                            '''The app only collects the teacher's name, email, password, and contact number during the sign-up process. However, this information is treated as a request, and it is not stored permanently in the database if the individual is not a teacher at that particular school. The data is sent to the admin as a request, and if the admin verifies that the individual is a teacher and accepts the request, only then is the data stored in the database. Otherwise, if the admin rejects the request, the data will be cleared.''',
+                            textAlign: TextAlign.justify),
                         Text(
                           '2.2 Students Data:',
                           style: TextStyle(
@@ -90,8 +98,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          '''Students or any other users are not providing any details directly to the app,they can only login if you are a student. Students personal details and credit details are already then given to the school administration and they have the authority to use the data. The authority is giving the data to teachers. They create the accounts for students and the password also ,so students or any other user can't login without the email and password. Students give email directly to teachers so they know the teacher and the school have their data. Teachers create and give the password to students so others can't use the app.''',
-                        ),
+                            '''Students or any other users do not directly provide any details to the app; they can only log in if they are a student. Personal details and credit information for students are already provided to the school administration, and they have the authority to use this data. The administration, in turn, is responsible for providing the data to teachers. Teachers are tasked with creating accounts for students and generating passwords. As a result, students or any other users cannot log in without the corresponding email and password. Students directly provide their email to teachers, ensuring that teachers are aware of the student and that the school possesses their data. Teachers are responsible for creating and distributing passwords to students to prevent unauthorized access to the app.''',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 8.0),
                         Text(
                           '2.3 School Administration Data:',
@@ -100,8 +108,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The App does not collect data related to the school administration, such as the name and contact details of the school principal or head of the school.They have only the control of the app. This is only for educational purposes.',
-                        ),
+                            'The app does not collect data related to the school administration, such as the name and contact details of the school principal or head of the school. They only have control over the app, and it is intended solely for educational purposes.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 16.0),
                         Text(
                           '3. Data Control:',
@@ -118,8 +126,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The collected data is under the control of the school administrator, who is typically the principal or head of the school. The administrator has exclusive access to and control over user data.',
-                        ),
+                            'The collected data is under the control of the school administrator, who is typically the principal or head of the school. The administrator has exclusive access to and control over user data.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 8.0),
                         Text(
                           '3.2 Developer Access:',
@@ -128,8 +136,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The Developer, FlutterVerseOfficial, does not have access to the user data collected by the App. All data is solely controlled and managed by the school administrator.',
-                        ),
+                            'The Developer, FlutterVerseOfficial, does not have access to the user data collected by the App. All data is solely controlled and managed by the school administrator.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 16.0),
                         Text(
                           '4. Data Usage:',
@@ -146,8 +154,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The collected data is used for educational purposes within the school environment. This includes facilitating communication between teachers and students, managing academic activities, and other educational functionalities.',
-                        ),
+                            'The collected data is used for educational purposes within the school environment. This includes facilitating communication between teachers and students, managing academic activities, and other educational functionalities.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 8.0),
                         Text(
                           '4.2 No Data Sharing:',
@@ -156,8 +164,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The App does not share user data with any external third parties, including the Developer. The data is strictly controlled within the school environment.',
-                        ),
+                            'The App does not share user data with any external third parties, including the Developer. The data is strictly controlled within the school environment.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 16.0),
                         Text(
                           '5. Data Security:',
@@ -174,8 +182,8 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'The App employs industry-standard encryption protocols to secure user data during transmission and storage.',
-                        ),
+                            'The App employs industry-standard encryption protocols to secure user data during transmission and storage.',
+                            textAlign: TextAlign.justify),
                         SizedBox(height: 16.0),
                         Text(
                           '6. Contact Information:',
@@ -192,7 +200,7 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'For any questions or concerns regarding privacy, you can contact School Admin at amaljvattakkunnel@gmail.com.',
+                          'For any questions or concerns regarding privacy, you can contact School Admin at : amaljvattakkunnel@gmail.com.',
                         ),
                         SizedBox(height: 8.0),
                         Text(
@@ -202,7 +210,7 @@ class ScreenFirstPrivacy extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'For any questions or concerns regarding privacy, you can contact FlutterVerseOfficial at flutterverseofficial@gmail.com.',
+                          'For any questions or concerns regarding privacy, you can contact FlutterVerseOfficial at :  flutterverseofficial@gmail.com.',
                         ),
                         SizedBox(height: 16.0),
                         Text(
@@ -265,6 +273,6 @@ class ScreenFirstPrivacy extends StatelessWidget {
           builder: (context) => ScreenFirst(),
         ));
     final sharedPrefs = await SharedPreferences.getInstance();
-    await sharedPrefs.setBool('privacy_policy', true);   
+    await sharedPrefs.setBool('privacy_policy', true);
   }
 }
